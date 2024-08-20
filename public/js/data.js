2c32a35501dedc0b90c662a53c1556737ecfb1fb
seanlanepgh <seanlane997@gmail.com>
2024-08-20 16:15:11 +0100

    Add Products Request / Get Data from Request not file

--- public/js/data.js
+++ public/js/data.js
@@ -1,64 +1,72 @@
-var data = $.getJSON("/data/product.json", function(json) {
+getData();
+
+function showData(productData) {
     var productList = document.getElementById('products');
-    for (var i = 0; i < json.product_arr.length; i++) {
-        var productItem = document.createElement("div");
-        productItem.setAttribute('class', 'item');
-        productItem.setAttribute('display', 'inline-block');
-        var img = document.createElement("img");
-        img.setAttribute('src', '/img/'+json.product_arr[i].img+'.jpg');
-        img.setAttribute('object-fit','contain');
-        productItem.appendChild(img);
-        var imgDiv = document.createElement("div");
-        imgDiv.setAttribute('class', 'product-image')
-        imgDiv.appendChild(img)
-        productItem.appendChild(imgDiv);
 
-        var name = document.createElement("h3");
-        name.setAttribute('class', 'name');
-        name.setAttribute('fontWeight', 'bold');
-        name.innerText = json.product_arr[i].name
-        productItem.appendChild(name);
+    productData.forEach((item) => {
+    var productItem = document.createElement("div");
+    productItem.setAttribute('class', 'item');
+    productItem.setAttribute('display', 'inline-block');
+    var img = document.createElement("img");
+    img.setAttribute('src', '/img/'+item.img+'.jpg');
+    img.setAttribute('object-fit','contain');
+    productItem.appendChild(img);
+    var imgDiv = document.createElement("div");
+    imgDiv.setAttribute('class', 'product-image')
+    imgDiv.appendChild(img)
+    productItem.appendChild(imgDiv);
 
-        var price = document.createElement("p");
-        price.setAttribute('class', 'price');
-        var priceValue = new Intl.NumberFormat('en-US', { style: 'currency', currency: 'GBP' }).format(
-            json.product_arr[i].price/100,
-        );
-        price.innerText = priceValue;
-        productItem.appendChild(price);
+    var name = document.createElement("h3");
+    name.setAttribute('class', 'name');
+    name.innerText = item.product_name
+    productItem.appendChild(name);
 
-        if(((typeof(json.product_arr[i].was_price) == "number"))){
-            var wasPrice = document.createElement("p");
-            wasPrice.setAttribute('class', 'wasPrice');
-            var wasPriceValue = new Intl.NumberFormat('en-US', { style: 'currency', currency: 'GBP' }).format(
-                json.product_arr[i].was_price/100,
-            );
-            wasPrice.innerHTML += 'Was <s>'+wasPriceValue+'</s>';
-            productItem.appendChild(wasPrice);
-        } else {
-            var wasPrice = document.createElement("br");
-            wasPrice.setAttribute('class', 'wasPrice');
-            productItem.appendChild(wasPrice);
+    var price = document.createElement("p");
+    price.setAttribute('class', 'price');
+    var priceValue = new Intl.NumberFormat('en-US', { style: 'currency', currency: 'GBP' }).format(
+        item.price,
+      );
+    price.innerText = priceValue;
+    productItem.appendChild(price);
 
-        }
-
-        if(!((typeof(json.product_arr[i].reviews) == "boolean"))){
-            var reviewScore = document.createElement("p");
-            reviewScore.setAttribute('class', 'reviewScore');
-            reviewScore.innerText = json.product_arr[i].reviews+'% Review Score';
-            productItem.appendChild(reviewScore);
-        }
-        else {
-            var reviewScore = document.createElement("br");
-            reviewScore.setAttribute('class', 'reviewScore');
-            productItem.appendChild(reviewScore);
-        }
-
-        var button = document.createElement("button");
-        button.setAttribute('class','button');
-        button.innerText= "Add To Basket";
-        productItem.appendChild(button);
-        productList.appendChild(productItem);
+   if(((typeof(item.was_price) == "number" && item.was_price !== 0 ))){
+        var wasPrice = document.createElement("p");
+        wasPrice.setAttribute('class', 'wasPrice');
+        var wasPriceValue = new Intl.NumberFormat('en-US', { style: 'currency', currency: 'GBP' }).format(
+            item.was_price,
+          );
+        wasPrice.innerHTML += 'Was <s>'+wasPriceValue+'</s>';
+        productItem.appendChild(wasPrice);
+    } else {
+        var wasPrice = document.createElement("br");
+        wasPrice.setAttribute('class', 'wasPrice');
+        productItem.appendChild(wasPrice);
 
     }
-});
+    if((typeof(item.reviews) == "number" && item.reviews !== 0)){
+        var reviewScore = document.createElement("p");
+        reviewScore.setAttribute('class', 'reviewScore');
+        reviewScore.innerText = item.reviews+'% Review Score';
+        productItem.appendChild(reviewScore);
+    }
+    else {
+        var reviewScore = document.createElement("br");
+        reviewScore.setAttribute('class', 'reviewScore');
+        productItem.appendChild(reviewScore);
+    }
+
+    var button = document.createElement("button");
+    button.setAttribute('class','button');
+    button.innerText= "Add To Basket";
+    productItem.appendChild(button);
+    productList.appendChild(productItem);
+    });
+};
+function getData() {
+    fetch("http://127.0.0.1:8000/api/products")
+    .then((response) => response.json())
+    .then((json) => {
+        console.log(json)
+        showData(json)
+    });
+}
--- public/js/filterButtons.js
+++ public/js/filterButtons.js
@@ -1,24 +1,27 @@
 
 function sortPrice() {
     removeProducts();
-    $.getJSON("/data/product.json", function(json) {
-        let indexedArray = json.product_arr.map((item, index) => (
+    fetch("http://127.0.0.1:8000/api/products")
+    .then((response) => response.json())
+    .then((json) => {
+        let indexedArray = json.map((item, index) => (
             { index, value: item }
         ));
         indexedArray.sort((a, b) => comparePrice(a,b));
         
         let sortedPrice = indexedArray.map((item) => item.value);
         showData(sortedPrice);
-    });    
+    }); 
 };
 
 function sortSavings() {
     removeProducts();
-    $.getJSON("/data/product.json", function(json) {
-        replaceBooleans(json);
-
-        let indexedArray = json.product_arr.map((item, index) => ({ index, value: item }));
-
+    fetch("http://127.0.0.1:8000/api/products")
+    .then((response) => response.json())
+    .then((json) => {
+        let indexedArray = json.map((item, index) => (
+            { index, value: item }
+        ));
         indexedArray.sort((a, b) => compareWasPrice(a,b));
         
         let sortedSavings = indexedArray.map((item) => item.value);
@@ -28,10 +31,12 @@ function sortSavings() {
 
 function sortReviews() {
     removeProducts();
-    $.getJSON("/data/product.json", function(json) {
-        replaceBooleans(json);
-
-        let indexedArray = json.product_arr.map((item, index) => ({ index, value: item }));
+    fetch("http://127.0.0.1:8000/api/products")
+    .then((response) => response.json())
+    .then((json) => {
+        let indexedArray = json.map((item, index) => (
+            { index, value: item }
+        ));
         indexedArray.sort((a, b) => compareReview(a,b));
         
         let sortedReviews = indexedArray.map((item) => item.value);
@@ -42,10 +47,12 @@ function sortReviews() {
 
 function sortName() {
     removeProducts();
-    $.getJSON("/data/product.json", function(json) {
-            replaceBooleans(json);
-            console.log(json)
-            let indexedArray = json.product_arr.map((item, index) => ({ index, value: item }));
+    fetch("http://127.0.0.1:8000/api/products")
+    .then((response) => response.json())
+    .then((json) => {
+        let indexedArray = json.map((item, index) => (
+            { index, value: item }
+        ));
             indexedArray.sort((a, b) => compareName(a,b));
 
         let sortedName = indexedArray.map((item) => item.value);
@@ -56,73 +63,9 @@ function sortName() {
 
 function getData() {
     var data = $.getJSON("/data/product.json", function(json) {
-        console.log(json); // this will show the info it in firebug console
         return data;
     });
 }
-
-function showData(productData) {
-    var productList = document.getElementById('products');
-
-    productData.forEach((item) => {
-    var productItem = document.createElement("div");
-    productItem.setAttribute('class', 'item');
-    productItem.setAttribute('display', 'inline-block');
-    var img = document.createElement("img");
-    img.setAttribute('src', '/img/'+item.img+'.jpg');
-    img.setAttribute('object-fit','contain');
-    productItem.appendChild(img);
-    var imgDiv = document.createElement("div");
-    imgDiv.setAttribute('class', 'product-image')
-    imgDiv.appendChild(img)
-    productItem.appendChild(imgDiv);
-
-    var name = document.createElement("h3");
-    name.setAttribute('class', 'name');
-    name.innerText = item.name
-    productItem.appendChild(name);
-
-    var price = document.createElement("p");
-    price.setAttribute('class', 'price');
-    var priceValue = new Intl.NumberFormat('en-US', { style: 'currency', currency: 'GBP' }).format(
-        item.price/100,
-      );
-    price.innerText = priceValue;
-    productItem.appendChild(price);
-
-   if(((typeof(item.was_price) == "number" && item.was_price !== 0 ))){
-        var wasPrice = document.createElement("p");
-        wasPrice.setAttribute('class', 'wasPrice');
-        var wasPriceValue = new Intl.NumberFormat('en-US', { style: 'currency', currency: 'GBP' }).format(
-            item.was_price/100,
-          );
-        wasPrice.innerHTML += 'Was <s>'+wasPriceValue+'</s>';
-        productItem.appendChild(wasPrice);
-    } else {
-        var wasPrice = document.createElement("br");
-        wasPrice.setAttribute('class', 'wasPrice');
-        productItem.appendChild(wasPrice);
-
-    }
-    if((typeof(item.reviews) == "number" && item.reviews !== 0)){
-        var reviewScore = document.createElement("p");
-        reviewScore.setAttribute('class', 'reviewScore');
-        reviewScore.innerText = item.reviews+'% Review Score';
-        productItem.appendChild(reviewScore);
-    }
-    else {
-        var reviewScore = document.createElement("br");
-        reviewScore.setAttribute('class', 'reviewScore');
-        productItem.appendChild(reviewScore);
-    }
-
-    var button = document.createElement("button");
-    button.setAttribute('class','button');
-    button.innerText= "Add To Basket";
-    productItem.appendChild(button);
-    productList.appendChild(productItem);
-    });
-};
 
 function removeProducts(){
     const parent = document.getElementById("products");
@@ -163,8 +106,8 @@ function compareReview(a, b) {
 }
 function compareName(a, b) {
     //Remove digits and symbols as they affect the sort
-    let nameA = a.value.name.replace(/[^A-Za-z]+/g, "").toUpperCase(); // ignore upper and lowercase
-    let nameB = b.value.name.replace(/[^A-Za-z]+/g, "").toUpperCase(); // ignore upper and lowercase
+    let nameA = a.value.product_name.replace(/[^A-Za-z]+/g, "").toUpperCase(); // ignore upper and lowercase
+    let nameB = b.value.product_name.replace(/[^A-Za-z]+/g, "").toUpperCase(); // ignore upper and lowercase
     if (nameA < nameB) {
         return -1;
     }
